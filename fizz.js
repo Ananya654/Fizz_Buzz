8b0e81dee44b27b6f4346350259aee1cf6d361ec
Ananya654 <ananyap654@gmail.com>
2024-03-21 01:58:26 +0530

    tst

--- fizz.js
+++ fizz.js
@@ -2,10 +2,10 @@
 let output = [];
 function fizzBuzz(){
     for(let i = 1; i<=100;i++ ){
-        if(i%3 === 0){
+        if(i%3 == 0 && i%5 !== 0){
             output.push("Fizz");
         }
-        else if(i%5 === 0){
+        else if(i%5 == 0 && i%3 !== 0){
             output.push("Buzz");
         }
         else if(i%3 === 0 && i%5 === 0){
